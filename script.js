abcdd500b580f0e326429f6d9fecabe9f36317e4
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-10-14 18:29:30 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+Prism.languages.adonis = {
+    'command': {
+        pattern: /:[a-zA-Z]+/,
+        greedy: true
+    },
+    'argument': {
+        pattern: /\s\S+/,
+        greedy: true
+    }
+};
